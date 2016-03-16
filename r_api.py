d54dc5230eed60e1d4cec9e3bd4634ab329013f2
Sebastien BENOIT <sb@42.fr>
2016-03-16 16:25:46 +0100

    prep update deal

--- r_api.py
+++ r_api.py
@@ -5,8 +5,8 @@ import json
 from utils import *
 
 def r_api(app, config, db, cursor):
-    @app.route('/api/devis', method='GET', name='devis')
-    @app.route('/api/devis/', method='GET', name='devis')
+    @app.route('/api/devis', method='GET', name='api_devis')
+    @app.route('/api/devis/', method='GET', name='api_devis')
     def api_devis(session):
         check_session(app, session)
         devis = db.get_devis(cursor)
@@ -20,8 +20,8 @@ def r_api(app, config, db, cursor):
         devis = db.get_devis(cursor, did)
         return dict(data=devis)
 
-    @app.route('/api/deals', method='GET', name='deals')
-    @app.route('/api/deals/', method='GET', name='deals')
+    @app.route('/api/deals', method='GET', name='api_deals')
+    @app.route('/api/deals/', method='GET', name='api_deals')
     def api_deals(session):
         check_session(app, session)
         deals = db.get_deals(cursor)
@@ -30,6 +30,13 @@ def r_api(app, config, db, cursor):
 
     @app.route('/api/deals/<did:int>', method='GET')
     def api_deals(did, session):
+        assert isinstance(did, int)
+        check_session(app, session)
+        deals = db.get_deals(cursor, did)
+        return dict(data=deals)
+
+    @app.route('/api/deal/<did:int>', method='POST', name='api_deal_update')
+    def api_deal_update(did, session):
         assert isinstance(did, int)
         check_session(app, session)
         deals = db.get_deals(cursor, did)
